84af1b1153b86bb000c6d5fecd6f5dfd79ea4f61
Ian Bell <ian.bell@nist.gov>
2019-04-23 09:19:56 -0600

    Bump to 1.0.0

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ class CMakeBuild(build_ext):
 
 setup(
     name='PyCEGO',
-    version='0.0.0',
+    version='1.0.0',
     author='Ian Bell',
     author_email='ian.bell@nist.gov',
     description='C++->python wrapper of C++11 Evolutionary Global Optimization (CEGO)',
